93c6f957658583fd096072e686686b821b8f6617
전윤호 Uknow <yoon6763@naver.com>
2025-01-14 00:04:07 +0900

    예제 프로젝트

--- main.go
+++ main.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	initState()
+
+	for i := 0; i < 100; i++ {
+		fmt.Println(i)
+		if i%2 == 0 {
+			fmt.Println("Even")
+		} else {
+			fmt.Println("Odd")
+		}
+	}
+}
+
+func initState() {
+	fmt.Println("Init")
 }
